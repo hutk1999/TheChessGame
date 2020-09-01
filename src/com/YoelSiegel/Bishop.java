3d4e167f1e23d557fe8db1822f3ba6d0fa5a8b6a
hutk1999 <yoelsiegel@gmail.com>
2020-09-01 18:05:52 +0300

    bishop is done

--- src/com/YoelSiegel/Bishop.java
+++ src/com/YoelSiegel/Bishop.java
@@ -1,7 +1,73 @@
 package com.YoelSiegel;
 
+import java.awt.*;
+
 public class Bishop extends Piece{
     public Bishop(PieceColor ecolor, int x, int y) {
         super(ecolor, x, y, PieceType.BISHOP);
+    }
+
+    public void getLegalMoves(ChessBoard temp){
+        for (int i = 1; i < 8; i++) {
+            if(betweenExclusive(getX()+i, 0, 8)&&betweenExclusive(getY()+i, 0, 8)){
+                if (temp.gettile(getX() + i, getY() + i).isPieceOn()) {
+                    if(temp.gettile(getX() + i, getY() + i).getPiece().getPieceColor()!=temp.gettile(getX(), getY()).getPiece().getPieceColor()){
+                        temp.getChessboard()[getX()+i][getY()+i].setTheBackground(Color.GREEN);
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                temp.getChessboard()[getX()+i][getY()+i].setTheBackground(Color.GREEN);
+            }
+        }
+        for (int i = 1; i < 8; i++) {
+            if(betweenExclusive(getX()-i, 0, 8)&&betweenExclusive(getY()-i, 0, 8)){
+                if (temp.gettile(getX() - i, getY() - i).isPieceOn()) {
+                    if(temp.gettile(getX() - i, getY() - i).getPiece().getPieceColor()!=temp.gettile(getX(), getY()).getPiece().getPieceColor()){
+                        temp.getChessboard()[getX()-i][getY()-i].setTheBackground(Color.GREEN);
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                temp.getChessboard()[getX()-i][getY()-i].setTheBackground(Color.GREEN);
+            }
+        }
+        for (int i = 1; i < 8; i++) {
+            if(betweenExclusive(getX()+i, 0, 8)&&betweenExclusive(getY()-i, 0, 8)){
+                if (temp.gettile(getX() + i, getY() - i).isPieceOn()) {
+                    if(temp.gettile(getX() + i, getY() - i).getPiece().getPieceColor()!=temp.gettile(getX(), getY()).getPiece().getPieceColor()){
+                        temp.getChessboard()[getX()+i][getY()-i].setTheBackground(Color.GREEN);
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                temp.getChessboard()[getX()+i][getY()-i].setTheBackground(Color.GREEN);
+            }
+        }
+        for (int i = 1; i < 8; i++) {
+            if(betweenExclusive(getX()-i, 0, 8)&&betweenExclusive(getY()+i, 0, 8)){
+                if (temp.gettile(getX() - i, getY() + i).isPieceOn()) {
+                    if(temp.gettile(getX() - i, getY() + i).getPiece().getPieceColor()!=temp.gettile(getX(), getY()).getPiece().getPieceColor()){
+                        temp.getChessboard()[getX()-i][getY()+i].setTheBackground(Color.GREEN);
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                temp.getChessboard()[getX()-i][getY()+i].setTheBackground(Color.GREEN);
+            }
+        }
+
+    }
+
+    public static boolean betweenExclusive(int x, int min, int max) {
+        return x>=min && x<max;
     }
 }
